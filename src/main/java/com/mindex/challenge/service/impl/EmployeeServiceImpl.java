a8acd5dddca06dca88c2c6e8cbc7ea317791f2bb
Marc Chametzky <marc@bluevine.net>
2023-07-18 09:04:44 -0400

    Intentional error being committed to main.

--- src/main/java/com/mindex/challenge/service/impl/EmployeeServiceImpl.java
+++ src/main/java/com/mindex/challenge/service/impl/EmployeeServiceImpl.java
@@ -31,7 +31,7 @@ public class EmployeeServiceImpl implements EmployeeService {
     public Employee read(String id) {
         LOG.debug("Creating employee with id [{}]", id);
 
-        Employee employee = employeeRepository.findByEmployeeId(id);
+        Employee employee = employeeRepository.findByEmployeeId(id2);
 
         if (employee == null) {
             throw new RuntimeException("Invalid employeeId: " + id);
